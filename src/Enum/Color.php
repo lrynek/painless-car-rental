41a6b6c29ef687ae8f3b0e8df19c717a98219a78
Łukasz Rynek <lukasz.rynek@docplanner.com>
2021-09-30 15:11:50 +0200

    Add more car images, colors and polish car card layout

--- src/Enum/Color.php
+++ src/Enum/Color.php
@@ -5,14 +5,20 @@ namespace App\Enum;
 
 /**
  * @method static self BLACK()
+ * @method static self BLUE()
+ * @method static self GREEN()
  * @method static self RED()
  * @method static self SILVER()
+ * @method static self WHITE()
  * @method static self YELLOW()
  */
 final class Color extends AbstractEnum
 {
 	private const BLACK = 'black';
+	private const BLUE = 'blue';
+	private const GREEN = 'green';
 	private const RED = 'red';
 	private const SILVER = 'silver';
+	private const WHITE = 'white';
 	private const YELLOW = 'yellow';
 }
