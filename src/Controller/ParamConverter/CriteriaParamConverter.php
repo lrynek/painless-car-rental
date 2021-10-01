08d777a7baf22094276f798308b9b3fd66221e04
Łukasz Rynek <lukasz.rynek@docplanner.com>
2021-10-01 12:34:03 +0200

    Add PhraseFilter

--- src/Controller/ParamConverter/CriteriaParamConverter.php
+++ src/Controller/ParamConverter/CriteriaParamConverter.php
@@ -5,13 +5,17 @@ namespace App\Controller\ParamConverter;
 
 use App\Repository\Elasticsearch\ValueObject\Criteria\ColorsFilter;
 use App\Repository\Elasticsearch\ValueObject\Criteria\Criteria;
+use App\Repository\Elasticsearch\ValueObject\Criteria\PhraseFilter;
 use App\ValueObject\Colors;
+use App\ValueObject\Phrase;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Component\HttpFoundation\Request;
 
 final class CriteriaParamConverter extends AbstractParamConverter
 {
 	private const PARAM_FILTERS = 'filters';
+
+	private const FILTER_PHRASE = 'phrase';
 	private const FILTER_COLORS = 'colors';
 
 	public function apply(Request $request, ParamConverter $configuration): bool
@@ -20,11 +24,23 @@ final class CriteriaParamConverter extends AbstractParamConverter
 		$filters = $request->get(self::PARAM_FILTERS, []);
 		$criteria = new Criteria;
 
+		$this->processPhrase($criteria, $filters);
 		$this->processColors($criteria, $filters);
 
 		$request->attributes->set($param, $criteria);
 
 		return true;
+	}
+
+	private function processPhrase(Criteria $criteria, mixed $filters): void
+	{
+		$phrase = $filters[self::FILTER_PHRASE] ?? '';
+
+		if (empty($phrase)) {
+			return;
+		}
+
+		$criteria->addRequired(new PhraseFilter(new Phrase($phrase)));
 	}
 
 	protected function processColors(Criteria $criteria, array $filters): void
---
+++ src/Repository/Elasticsearch/ValueObject/Criteria/PhraseFilter.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Repository\Elasticsearch\ValueObject\Criteria;
+
+use App\ValueObject\Phrase;
+
+final class PhraseFilter implements Criterion
+{
+	public function __construct(private Phrase $phrase)
+	{
+	}
+
+	public function definition(): array
+	{
+		return [
+			'query_string' => [
+				'query' => $this->phrase->value(),
+				'fields' => [
+					'producer',
+					'model',
+				]
+			]
+		];
+	}
+}
