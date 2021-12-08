327b2917af216b37ab713cc7bc02b3a47a2297b9
Łukasz Rynek <lukasz.rynek@docplanner.com>
2021-12-08 19:41:33 +0100

    Code style

--- src/Elasticsearch/ValueObject/Factor/ColorRelevanceFactor.php
+++ src/Elasticsearch/ValueObject/Factor/ColorRelevanceFactor.php
@@ -26,29 +26,29 @@ final class ColorRelevanceFactor extends WeightFactor
 	{
 		return <<<JS
 			if (params.requiredColors.isEmpty()) {
-    			return 0;
+				return 0;
 			}
 
 			if (false === doc['colors'].containsAll(params.requiredColors)) {
-    			return 0;
+				return 0;
 			}
 
-            def colorExclusivnessFactor = (params.maximumColorsPerDocument + 1.0 - doc['colors'].size()) / params.maximumColorsPerDocument * 0.25;
+			def colorExclusivnessFactor = (params.maximumColorsPerDocument + 1.0 - doc['colors'].size()) / params.maximumColorsPerDocument * 0.25;
 
-            def requiredColorsCount = params.requiredColors.size();
+			def requiredColorsCount = params.requiredColors.size();
 			def positionsFactorSum = params.requiredColors
 				.collect( requiredColor -> {
 					if (false === doc['colors'].contains(requiredColor)) {	
 						return 0;
 					}
 
-                    def position = params._source['colors'].indexOf(requiredColor) + 1.0;
+					def position = params._source['colors'].indexOf(requiredColor) + 1.0;
 
 					return (params.maximumColorsPerDocument + 1.0 - position) / params.maximumColorsPerDocument;
 				})
 				.sum() * 0.75;
 
-            def colorPositionFactor = positionsFactorSum / requiredColorsCount;
+			def colorPositionFactor = positionsFactorSum / requiredColorsCount;
 
 			return (colorExclusivnessFactor + colorPositionFactor);
 JS;
